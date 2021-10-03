87b7277002fccc542c9a2fa9598a33ddbb281964
hwany <trin_@naver.com>
2021-10-03 20:16:02 +0900

    new project init

--- jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,10 +1,10 @@
-package hello.hellojpa;
+package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloJpaApplicationTests {
+class JpashopApplicationTests {
 
 	@Test
 	void contextLoads() {
